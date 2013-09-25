71eef89d853213c23925e7d624ea8d29bc47d9da
Robbie McMahon <mcmahon.robbie.spam@gmail.com>
2013-09-24 20:51:30 -0700

    Added cursor movement
    Now depends on ansi-terminal.
    The cursor can move, but it leaves behind characters.
    Need to redraw the board every time.

--- tictactoe/Main.hs
+++ tictactoe/Main.hs
@@ -1,12 +1,35 @@
 import System.Console.Haskeline
+import System.Console.ANSI
+import System.IO
+import Control.Monad.Trans
 
-main = runInputT defaultSettings loop
-    where 
-        loop :: InputT IO ()
-        loop = do
-            minput <- getInputChar "% "
-            case minput of
-                Nothing -> return ()
-                Just 'q' -> return ()
-                Just input -> do outputStrLn $ "Input was: " ++ [input]
-                                 loop
+
+prompt = do
+        c <- getInputChar ""
+        case c of
+            Just 'q' -> return False
+            Just 'w' -> do
+                        liftIO $ cursorUpLine 2
+                        prompt
+            Just 's' -> do
+                        liftIO $ cursorDownLine 1
+                        prompt
+            _ -> return False
+
+loop = do
+    result <- prompt
+    if result then loop else return ()
+
+printRow = outputStrLn ".|.|."
+printSeparator = outputStrLn "- - -"
+    
+setupBoard = do
+    printRow
+    printSeparator
+    printRow
+    printSeparator
+    printRow
+    
+main = do
+    runInputT defaultSettings setupBoard
+    runInputT defaultSettings loop
